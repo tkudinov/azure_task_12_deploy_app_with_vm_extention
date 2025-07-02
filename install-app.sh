9930cf2038a722bc8ad88fad8dc5a38e2da0c3c8
tkudinov <tkudinov90@gmail.com>
2025-07-02 18:09:18 +0300

    Used VM extension to automate the deployment of todoapp app to the VM. Changed install-app.sh.

--- install-app.sh
+++ install-app.sh
@@ -13,7 +13,7 @@ apt-get install python3-pip -yq
 # Create a directory for the app and download the files. 
 mkdir /app 
 # make sure to uncomment the line bellow and update the link with your GitHub username
-git clone https://raw.githubusercontent.com/tkudinov/azure_task_12_deploy_app_with_vm_extention/main/install-app.sh
+#git clone https://raw.githubusercontent.com/tkudinov/azure_task_12_deploy_app_with_vm_extention/main/install-app.sh
 cp -r azure_task_12_deploy_app_with_vm_extention/app/* /app
 
 # create a service for the app via systemctl and start the app
